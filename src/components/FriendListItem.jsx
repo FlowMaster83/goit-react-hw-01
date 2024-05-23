8121a7e711fb642dbbfa4be10c03b9baaacd101d
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2024-05-23 14:26:17 +0300

    Update FriendListItem.jsx

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -6,6 +6,7 @@ export default function FriendListItem({
       <img src={avatar} alt="Avatar" />
       <p>{name}</p>
       <p>{isOnline ? "Online" : "Offline"}</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ut eius veniam quibusdam iste molestias ad sunt quam porro. Aperiam aliquid dolorum natus dignissimos consectetur voluptatum aut atque sapiente facere.</p>
     </div>
   );
 }
